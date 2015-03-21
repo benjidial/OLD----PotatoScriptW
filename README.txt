22254b4e9040c7ceb0e2a8d5ef86ed35695ebbf4
Benji <benji3.141@gmail.com>
2015-03-21 01:53:42 -0400

    Alpha 0.1.3.2
    Added "log" and "beep" commands.
    Added "All Commands.psw" sample file.
    Added picture.
    Changed some minor things.

--- README.txt
+++ README.txt
@@ -3,5 +3,9 @@ Write it in a text editor of your choice.
 Click and drag to the .exe.
 
 Changelog:
+Alpha 0.1.3.2
+Added "log" and "beep" Commands
+Added "All Commands.psw" Sample File
+
 Alpha 0.1.2.2
 First public version
--- Source.cpp
+++ Source.cpp
@@ -1,65 +1,75 @@
 #include <iostream>  // Console IO
 #include <fstream>   // File IO
 #include <string>    // Easier Strings
-#include <math.h>    // exponents
-void COOLSTUFF(char *file);
-int main(int argc, char *argv[ ]) // Program enters here.
+template <typename Type>
+void COOLSTUFF(Type file);
+int main(int argc, char *argv[]) // Program enters here.
 {
 	using namespace std; // Makes some code shorter.
-	if (argc == 1)
+	int nFiles = argc - 1;
+	if (!nFiles)
 	{
 		cout << "No file supplied!" << endl;
-		cout << "Try clicking and dragging a file to this one." << endl;
+		cout << "Press enter to close this window." << endl;
 		cin.clear();
 		cin.get();
-		exit(-1);
 	}
-	for (int nArg = 1; nArg < argc; nArg++)  // Allows multiple files.
+	for (int nArg = 1; nArg <= nFiles; nArg++)  // Allows multiple files.
 	{
 		ofstream outf("logfile.txt");
-		outf << "Begin processing file " << nArg <<
-			" of " << argc - 1 << ": " << argv[nArg] << endl;
-
+		outf << "Begin processing file " << nArg << " of " << nFiles << ": \"" << argv[nArg] << "\"" << endl;
+		outf.flush();
 		try
 		{
+			outf.close();
 			COOLSTUFF(argv[nArg]);
 		}
 		catch (char* strException)
 		{
+			outf.open("logfile.txt", ios::app);
 			outf << strException << endl;
+			outf.close();
 		}
-		outf << "Finished processing file " << nArg <<
-			" of " << argc - 1 << ": " << argv[nArg] << endl;
-		if (nArg == argc - 1)
+		outf.open("logfile.txt", ios::app);
+		outf << "Finished processing file " << nArg << " of " << nFiles << ": \"" << argv[nArg] << "\"" << endl;
+		outf.flush();
+		if (nArg == nFiles)
 		{
 			cout << "Finished all interpereting jobs!" << endl;
 			cout << "Press enter to close this window." << endl;
 		}
 		else
 		{
-			cout << "Finished interpeteting job " << nArg <<
-				" of " << argc - 1 << "." << endl;
-			cout << "Press enter to start nex job." << endl;
+			cout << "Finished interpeteting job " << nArg << " of " << nFiles << "." << endl;
+			cout << "Press enter to start next job." << endl;
 		}
 		cin.clear();
 		cin.get();
 	}
 	return 0;  // Lets the OS know there were no problems.
 }
-void COOLSTUFF(char *file)
+
+template <typename Type>
+void COOLSTUFF(Type file)
 {
 	using namespace std;
+	string strQ;
 	ifstream inf(file);
 	if (!inf)
 		throw "Could not open file.";
+	ofstream outf("logfile.txt", ios::app);
 	while (inf)
 	{
-		string strQ;
+		cin.clear();
 		inf >> strQ;
-		if (strQ == "alert")
+		if (strQ == "alert") // logs
 		{
-			cout << static_cast<char>(7);
+			cout << static_cast<char>(7); // bell
+			outf << "Alert triggered." << endl;
+			outf.flush();
 		}
+		else if (strQ == "beep")  // doesn't log
+			cout << static_cast<char>(7);
 		else if (strQ == "clearscreen")
 			cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
 		else if (strQ == "display")
@@ -67,15 +77,24 @@ void COOLSTUFF(char *file)
 			getline(inf, strQ);
 			cout << strQ << endl;
 		}
-		
 		else if (strQ == "exit")
-			throw "Hit exit. (This is harmless.)";
+		{
+			outf << "Hit exit. (This is probably harmless.)" << endl;
+			outf.close();
+			break;
+		}
+		else if (strQ == "log")
+		{
+			getline(inf, strQ);
+			outf << strQ << endl;
+			outf.flush();
+		}
 		else if (strQ == "wait")
 		{
 			cin.clear();
 			cin.get();
 		}
 		else
-			throw "Unrecognized command.";
+			outf << "Unrecognized command or blank line: \"" << strQ << "\"" << endl;
 	}
 }
